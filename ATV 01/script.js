97b8ffe27bff0ab5743201067ab9de2e96e239e6
Murilo Ribeiro <160795915+omuriloribeiro@users.noreply.github.com>
2024-08-30 23:27:17 +0000

    conclusão da atividade

---
+++ ATV 01/script.js
@@ -0,0 +1,7 @@
+function mudarCor(element, color){
+    var paragrafo = document.getElementById('parag1');
+    paragrafo.style.color = '#'+Math.floor(Math.random()*16777215).toString(16); 
+    // Código genial que achei na internet,  (16777215 == ffffff in decimal) depois ele vai multiplicando de forma aleatória
+
+}
+
